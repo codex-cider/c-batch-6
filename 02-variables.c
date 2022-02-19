00ae699b800201e8e142d4fbe4e56a58925c88b7
Krishnakant Madhpuriya <kkmadhpuriya@gmail.com>
2022-02-19 20:01:40 +0530

    C Start

---
+++ 02-variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    int xyz;  // Variable declartion
+    xyz = 20; // initialization + assignment
+
+    printf("----   xyz is %d  ------", xyz);
+    xyz = 50;
+    printf("\n------------------------\n");
+
+    printf("xyz is %d", xyz);
+}
