bccaf1e39851bb2670ad1fcd0dcd322de9bed65b
Krishnakant Madhpuriya <kkmadhpuriya@gmail.com>
2022-04-18 09:34:46 +0530

    Call by value & Call by reference

---
+++ CPP Programs/07-call-by-reference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void myFunction(int *a)
+{
+    cout << "Value of a inside myFunction is " << *a << endl;
+    *a = 30;
+}
+
+int main()
+{
+    int a = 20;
+    cout << "value of a before fn call " << a << endl;
+    myFunction(&a);
+    cout << "value of a after fn call " << a << endl;
+}
